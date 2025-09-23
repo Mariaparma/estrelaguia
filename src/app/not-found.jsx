ba410835c5a8f75b765c5fcd93dcb751415a790d
maria parma <mariaparma.tds1.senai@gmail.com>
2025-09-23 11:38:52 -0300

    fix: update in not found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -8,6 +8,7 @@ export default function NotFound() {
       <div className={styles.box}>
         <h1 className={styles.title}>404</h1>
         <p className={styles.message}>Página não encontrada.</p>
+        <p className={styles.message}>O conteúdo dessa página não existe. Volte para a Home!</p>
         <Link href="/" className={styles.button}>
           Voltar para Home
         </Link>
