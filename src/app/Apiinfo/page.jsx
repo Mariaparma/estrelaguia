4450e395a249ed97aab8c3db775ff37a8db772d0
maria parma <mariaparma.tds1.senai@gmail.com>
2025-09-23 11:39:54 -0300

    fix: update Apiinfo

--- src/app/Apiinfo/page.jsx
+++ src/app/Apiinfo/page.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import styles from './Apiinfo.module.css';
-
+import Header from '../../components/Header'; 
 const HomePage = () => {
   return (
     <div className={`${styles.global} ${styles.homepage}`}>
-      <header className={styles['homepage-header']}>
-        <div className={styles['logo-container']}>
-          <img src="/icons/estrelaguia.png" alt="Estrela Guia Logo" className={styles.logo} />
-        </div>
-        <nav>
-          <ul className={styles['nav-menu']}>
-            <li><a href="/Paises">Países</a></li>
-            <li><a href="/Roteiro">Roteiro</a></li>
-            <li><a href="/contato">Gastronomia</a></li>
-            <li><a href="/contato">Contato</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Header /> 
+
       <main className={styles['homepage-main']}>
         <section className="intro">
-        <h3>Bem-vindo ao Estrela Guia</h3>
-        <p>Descubra conteúdos incríveis e explore novas possibilidades.</p>
+          <h3>Bem-vindo ao Estrela Guia</h3>
+          <p>Descubra conteúdos incríveis e explore novas possibilidades.</p>
         </section>
+
         <section className={styles.cards}>
           <div className={styles.card}>
             <div className={styles['card-image-container']}>
@@ -30,13 +20,12 @@ const HomePage = () => {
             <div className={styles['card-content']}>
               <h4 className={styles['card-title']}>Explore o melhor país pra você fazer sua viagem dos sonhos </h4>
               <h4 className={styles['card-title']}>Descubra a população, moeda, localização e entre outros... </h4>
-              <a href="/Paises" className={styles['card-button']}>Descubra Mais</a>
+              <a href="/Entidade" className={styles['card-button']}>Descubra Mais</a>
             </div>
-            
+
             <div className={styles['card-image-container']}>
               <img src="/images/destino2.png" alt="Imagem Genérica" className={styles['card-image']} />
             </div>
-
             <div className={styles['card-content']}>
               <h4 className={styles['card-title']}>Explore o melhor país pra você fazer sua viagem dos sonhos </h4>
               <h4 className={styles['card-title']}>Descubra os melhores pontos turisticos pra fazer sua viagem mais especial </h4>
@@ -46,16 +35,15 @@ const HomePage = () => {
             <div className={styles['card-image-container']}>
               <img src="/images/destino3.png" alt="Imagem Genérica" className={styles['card-image']} />
             </div>
-
             <div className={styles['card-content']}>
               <h4 className={styles['card-title']}>Explore o melhor país pra você fazer sua viagem dos sonhos </h4>
               <h4 className={styles['card-title']}>Descubra a melhor gastronomia da sua viagem dos sonhos </h4>
               <a href="/Paises" className={styles['card-button']}>Descubra Mais</a>
             </div>
-
           </div>
         </section>
       </main>
+
       <footer className={styles['homepage-footer']}>
         <p>&copy; 2025 Estrela Guia. Todos os direitos reservados.</p>
       </footer>
